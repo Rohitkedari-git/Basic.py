63678449fded594cca1fb12ccf94f2af95111576
Rohit Kedari <rohitkedari017@gmail.com>
2026-09-02 12:02:32 +0530

    Add output examples and results to Numpy3.py

--- Numpy3.py
+++ Numpy3.py
@@ -34,3 +34,30 @@ print("\nOriginal Matrix: ")
 print(matrix)
 print("\nMatrix after adding 10 using broadcasting: ")
 print(result)
+
+
+"""++++++++++++++++++Output+++++++++++++++++++++
+
+Array A:  [10 20 30 40 50]
+Array B:  [1 2 3 4 5]
+Addition using np.add():  [11 22 33 44 55]
+Numbers:  [ 1  4  9 16 25]
+Square root using np.sqrt():  [1. 2. 3. 4. 5.]
+
+Array X:  [0 1 2 3]
+Exponential using np.exp():  [ 1.          2.71828183  7.3890561  20.08553692]
+
+Original marks:  [50 60 70 80 90]
+Marks after adding 5:  [55 65 75 85 95]
+
+Original Matrix: 
+[[10 20 30]
+ [40 50 60]
+ [70 80 90]]
+
+Matrix after adding 10 using broadcasting: 
+[[ 20  30  40]
+ [ 50  60  70]
+ [ 80  90 100]]
+
+=== Code Execution Successful ==="""
